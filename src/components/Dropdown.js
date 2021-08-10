a4a2928778bd75aa4e11d134b38aba8dcf1db919
Dan Shawl <danielrshawl@gmail.com>
2021-08-10 15:06:06 -0400

    Added logic that uses useRef to determine dropdown visibility

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 //  Creating a dropdown
 //    1.  Create an array of objects that are our options
 //    2.  create a Dropdown functional component that accepts the options
@@ -19,12 +19,42 @@ import React, { useState } from 'react';
 
 // bubble, slowness,
 
+//  Gesture based house/apartment searching app
+//  Maybe apply that to fitness somehow?
+
+/*
+useEffect(() => {
+  document.body.addEventListener(
+    "click", 
+    () => {
+      setOpen(false);
+    },
+    {capture: true} 
+  )
+}, [])
+*/
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+        // console.log(event.target);
+        setOpen(false);
+      }
+      // { capture: true }
+    );
+  }, []);
   const renderedOptions = options.map((option) => {
-    console.log(option.value, selected.value);
+    // console.log(option.value, selected.value);
     if (option.value === selected.value) {
       return null;
+      // if the option value = selected value, render nothing
     }
     return (
       <div
@@ -32,12 +62,14 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         key={option.value}
         onClick={() => onSelectedChange(option)}
       >
+        {/* returns a div that will change the state of the selected option on click*/}
         {option.label}
       </div>
     );
   });
+  // console.log(ref.current);
   return (
-    <div className="ui form container">
+    <div ref={ref} className="ui form container">
       <div className="field">
         <label htmlFor="" className="label">
           Select a Color
@@ -58,3 +90,19 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 };
 
 export default Dropdown;
+
+//  Event Bubbling -------------------------------------------------------
+//  When a div is clicked on that has an event handler attached, an event object is created
+//  The event object then travels up to the next parent element
+//  if the parent element has a click event handler, it is automatically invoked
+// then, it gos to the next parent element
+// THIS IS THE REASON FOR THE MENU OPENING AND CLOSING
+
+//  Two scenarios:
+//  1. We dont want the body event listener to do anything if we click inside the ddropdown
+//  2.  If we click outside of the dropdown, we want the body event listener to open/close the dropdown
+
+//  How do we figure out what element was clicked?
+//    - use event.target with any event handler (make sure its passed in)
+//  How do we figure out if that element was in the dropdown
+//    - useRef
